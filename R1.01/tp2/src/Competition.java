890c36c19a61ee7df01f23f882e3c86c3adc9067
unelDev <enee.prog@gmail.com>
2023-09-20 09:50:37 +0200

    Competition v2

--- R1.01/tp2/src/Competition.java
+++ R1.01/tp2/src/Competition.java
@@ -9,13 +9,18 @@ public class Competition {
         float minTime = Float.MAX_VALUE;
         float accruedTime = 0f;
         in.nextLine();
-        for (int i = 0; i < nbJudge; i++) {
+        for (int i = 1; i <= nbJudge; i++) {
             System.out.print("judge "+ i + " how many time for this candidate?");
             final float temp = in.nextInt();
             in.nextLine();
-            if(maxTime < temp) { maxTime = temp; }
-            else if(minTime > temp) { minTime = temp;}
-            accruedTime+=temp;
+            if(temp>20 || temp<0){
+                i--;
+                System.out.println("time must be between 0 and 20");
+            }else{
+                if(maxTime < temp) { maxTime = temp; }
+                else if(minTime > temp) { minTime = temp;}
+                accruedTime+=temp;
+            }
         }
     }
 }
