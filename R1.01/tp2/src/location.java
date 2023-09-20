044fe85252cd677cc64f1bddab9aa8a84e4a5ab5
unelDev <enee.prog@gmail.com>
2023-09-20 09:31:13 +0200

    calendar V2

--- R1.01/tp2/src/location.java
+++ R1.01/tp2/src/location.java
@@ -3,32 +3,50 @@ import java.util.Scanner;
 public class location {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("enter x location (int)");
-        int x = in.nextInt();
-        in.nextLine();
-        System.out.print("enter y location (int)");
-        int y = in.nextInt();
-        in.nextLine();
-        if (y > 0)
-            if (x == 0)
-                System.out.println("North");
-            else if(x > 0)
-                System.out.println("North-est");
+        boolean game = true;
+        while (game)
+        {
+
+            System.out.print("enter x location (int)");
+            final int x = in.nextInt();
+            in.nextLine();
+            System.out.print("enter y location (int)");
+            final int y = in.nextInt();
+            in.nextLine();
+            if (y > 0) {
+                if (x == 0) {
+                    System.out.println("North");
+                }
+                else if(x > 0) {
+                    System.out.println("North-est");
+                }
+                else {
+                    System.out.println("North_west");
+                }
+            }
+            else if(y < 0) {
+                if(x == 0) {
+                    System.out.println("Sud");
+                }
+                else if(x > 0) {
+                    System.out.println("Sud-est");
+                }
+                else {
+                    System.out.println("Sud-west");
+                }
+            }
             else
-                System.out.println("North_west");
-        else if(y < 0)
-            if(x == 0)
-                System.out.println("Sud");
-            else if(x > 0)
-                System.out.println("Sud-est");
-            else
-                System.out.println("Sud-west");
-        else
-            if(x > 0)
+            if(x > 0) {
                 System.out.println("Est");
-            else if (x < 0)
+            }
+            else if (x < 0) {
                 System.out.println("West");
+            }
             else
                 System.out.println("center");
+
+            System.out.print("do you want continue? (y | other)");
+            game = in.next().charAt(0) == 'y';
+        }
     }
 }
