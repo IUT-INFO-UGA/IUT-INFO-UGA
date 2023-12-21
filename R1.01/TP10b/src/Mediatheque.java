55fe8503024c4b3caf1c7459488eb9b320b14304
unelDev <enee.prog@gmail.com>
2023-12-21 09:41:35 +0100

    add TP12

--- R1.01/TP10b/src/Mediatheque.java
+++
@@ -1,8 +0,0 @@
-public class Mediatheque {
-	public static void main(String[] args) {
-		ListeTrieeC<Categorie> lCategories = new ListeTrieeC<>();
-		Categorie casserolerie = new Categorie(), vaisselle = new Categorie();
-		lCategories.insereTrie(casserolerie);
-
-	}
-}
